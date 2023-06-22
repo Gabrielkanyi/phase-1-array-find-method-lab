c6da34c2fc24e4d7408bc6c1cb99abf36579d4ce
Gabrielkanyi <gabriel.kanyi@student.moringaschool.comm>
2023-06-22 13:19:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin (group){
+  for(const item of group){
+    if(item.result === "W"){
+      return item.year
+    {
+    return undefined
+      }
+  }
+  }
+}
+
+record.find(superbowlWin)
